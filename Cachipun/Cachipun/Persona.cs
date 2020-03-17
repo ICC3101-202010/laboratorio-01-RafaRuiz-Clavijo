9cb4e953e729e20306572cf8c6cadde7f83ab325
Rafael Ruiz-Clavijo <rruizclavijo@miuandes.cl>
2020-03-17 16:16:28 -0300

    First commit of the program

---
+++ Cachipun/Cachipun/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cachipun
+{
+    public class Persona
+    {
+        private string name;
+        private string apellido;
+
+        public Persona(string n, string a)
+        {
+            name = n;
+            apellido = a;
+        }
+        public int Lanzar()
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, 3);
+            return a;
+        }
+
+        //Estas funciones estan para verificar que todo funciona
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public string getLastName()
+        {
+            return this.apellido;
+        }
+    }
+}
---
+++ Cachipun/Cachipun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cachipun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona jugador = new Persona("Bob","Kunga"); //Constructor -> Indica nombre y apellido
+            int play = jugador.Lanzar();                  //Comportamiento
+            Console.WriteLine(jugador.getName());         //Verificar que todo funciona
+            Console.WriteLine(jugador.getLastName());
+            Console.WriteLine("Juego: " + play);
+
+        }
+    }
+}
