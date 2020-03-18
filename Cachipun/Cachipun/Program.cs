4cfb30b8d7e43cf6b83d65f09a3455251fbf18ca
Rafael Ruiz-Clavijo <rruizclavijo@miuandes.cl>
2020-03-18 15:57:23 -0300

    Programa ver 1.2 -> Cambio de formato

--- Cachipun/Cachipun/Program.cs
+++ Cachipun/Cachipun/Program.cs
@@ -10,11 +10,10 @@ namespace Cachipun
     {
         static void Main(string[] args)
         {
-            Persona jugador = new Persona("Bob", "Kunga"); //Constructor -> Indica nombre y apellido
-            int play = jugador.Lanzar();                  //Comportamiento
+            Persona jugador = new Persona("Bob", "Kunga"); //Constructor -> Indica nombre y apellido               
             Console.WriteLine(jugador.getName());         //Verificar que todo funciona
             Console.WriteLine(jugador.getLastName());
-            Console.WriteLine("Juego: " + play);
+            Console.WriteLine("Juego: " + jugador.Lanzar()); //Comportamiento
         }
     }
 }
